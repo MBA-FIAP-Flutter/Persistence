dd7097184b6e885d19d1036bdc6ffcb9ea713987
Ricardo da Silva Ogliari <rogliariping@gmail.com>
2024-05-06 23:19:50 -0300

    done

--- lib/dog.dart
+++ lib/dog.dart
@@ -1,11 +1,28 @@
 class Dog {
-  final int id;
+  final int? id;
   final String name;
   final int age;
 
   const Dog({
-    required this.id,
+    this.id,
     required this.name,
     required this.age,
   });
+
+  // Convert a Dog into a Map. The keys must correspond to the names of the
+  // columns in the database.
+  Map<String, Object?> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'age': age,
+    };
+  }
+
+  // Implement toString to make it easier to see information about
+  // each dog when using the print statement.
+  @override
+  String toString() {
+    return 'Dog{id: $id, name: $name, age: $age}';
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:study_persistence/dog.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -35,6 +36,11 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  Database? database;
+
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _ageController = TextEditingController();
+
   @override
   void initState() {
     super.initState();
@@ -42,11 +48,11 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void _openDatabase() async {
-    final database = openDatabase(
+    database = await openDatabase(
       join(await getDatabasesPath(), 'doggie_database.db'),
       onCreate: (db, version) {
         return db.execute(
-          'CREATE TABLE dogs(id INTEGER PRIMARY KEY, name TEXT, age INTEGER)',
+          'CREATE TABLE dogs(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, age INTEGER)',
         );
       },
       version: 1,
@@ -60,16 +66,53 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: const Center(
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              '...',
-            ),
-          ],
-        ),
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Name',
+                  hintText: 'Enter a name',
+                ),
+                controller: _nameController,
+              ),
+              const SizedBox(height: 16,),
+              TextField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Age',
+                  hintText: 'Enter a age',
+                ),
+                keyboardType: TextInputType.number,
+                controller: _ageController,
+              ),
+              const SizedBox(height: 24,),
+              TextButton(
+                onPressed: (){
+                  Dog dog = Dog(
+                      name: _nameController.text,
+                      age: int.parse(_ageController.text));
+                  insertDog(dog);
+                  _nameController.text = '';
+                  _ageController.text = '';
+                },
+                child: const Text('Save')
+              )
+            ],
+          ),
       ),
+    );
+  }
+
+  // Define a function that inserts dogs into the database
+  Future<void> insertDog(Dog dog) async {
+    await database?.insert(
+      'dogs',
+      dog.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
     );
   }
 }
